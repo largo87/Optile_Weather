56a7f4a34330c53d4fbb155b5928a1179add6c47
Hamdi Taboubi <hamdi.taboubi@gmail.com>
2019-11-12 16:21:41 +0100

    Update ContainerApp.js

--- src/ContainerApp.js
+++ src/ContainerApp.js
@@ -12,15 +12,17 @@ class ContainerApp extends React.Component {
 		super(props);
 		this.state = {
       showComponent: false,
+      checked:true
      
 		}
 		this.showComponents = this.showComponents.bind(this);
 	}
 
-	showComponents() {
+	showComponents(e) {
     
 		this.setState({
       showComponent:!this.state.showComponent,
+      checked:e.target.checked
       
 		});
 	}
@@ -28,13 +30,28 @@ class ContainerApp extends React.Component {
     this.props.dispatch(fetchProducts());
   
   }
+ 
+  getDateRange(fullDate, i){ 
+    fullDate = fullDate.split(' ');
+    var date = fullDate[0].split(/\//);  
+    var newDate =  date[0] +" "+fullDate[1];
+    var k = new Date(newDate);
+    k = k.getFullYear() +"-"+ (k.getMonth() + 1) +"-"+ k.getDate() + " "+fullDate[1]
+    return k ;
+  }
+  getCheckDay(i){
+    let checkDay = new Date();
+    checkDay = checkDay.getFullYear() +"-"+ (checkDay.getMonth() + 1) +"-"+ (checkDay.getDate() + i) + " 21:00:00";
+    return checkDay;
+  }
+  
+  
 
   render() {
     const { error, loading, products } = this.props;
-    
-  
+   
     if (error) {
-      return <div>Error! {error.message}</div>;
+      return <div>Error Fetch JSON! {error.message}</div>;
     }
 
     if (loading) {
@@ -44,19 +61,21 @@ class ContainerApp extends React.Component {
 
     return (
       <div>          
-        <CheckboxGroup showComponents={this.showComponents} />
+        <CheckboxGroup showComponents={this.showComponents} checked={this.state.checked}/>
         <div className="row">
-      {products.slice(0, 3).map( lista =>
-         
-            <div className="column">
-            {this.state.showComponent ?  <WeatherCard key={lista.dt} temperature={Math.round(lista.main.temp * 9/5 - 459.67) + ' °F' } date={lista.dt_txt}/> : <WeatherCard key={lista.dt} temperature={Math.round(lista.main.temp - 273.15) + ' °C' } date={lista.dt_txt}/>}
-             
-            </div> 
+      {products.filter( data => this.getDateRange(data.dt_txt) === this.getCheckDay(0) || this.getDateRange(data.dt_txt) === this.getCheckDay(1) || this.getDateRange(data.dt_txt) === this.getCheckDay(2)).map( lista =>
+        <div className="column">
+            {this.state.showComponent ? 
+             <WeatherCard key={lista.dt} temperature={Math.round(lista.main.temp * 9/5 - 459.67) + ' °F' } date={lista.dt_txt.split(" ")[0]}/> :
+             <WeatherCard key={lista.dt} temperature={Math.round(lista.main.temp - 273.15) + ' °C' } date={lista.dt_txt.split(" ")[0]}/>}
+        </div> 
           
        )} 
         </div>
-        {this.state.showComponent ?  <BarGraph label={products.slice(0, 8).map( lista => Math.round(lista.main.temp * 9/5 - 459.67) + ' °F')}  axis = { products.slice(0, 8).map( lista => lista.main.temp * 9/5 - 459.67)}/> : <BarGraph label={products.slice(0, 8).map( lista => Math.round(lista.main.temp - 273.15) + ' °C')}  axis = { products.slice(0, 8).map( lista => lista.main.temp - 273.15)}/>}               
-      </div>
+            {this.state.showComponent ?
+             <BarGraph label={products.slice(0, 8).map( lista => Math.round(lista.main.temp * 9/5 - 459.67) + ' °F')}  axis = { products.slice(0, 8).map( lista => lista.main.temp * 9/5 - 459.67)}/> :
+             <BarGraph label={products.slice(0, 8).map( lista => Math.round(lista.main.temp - 273.15) + ' °C')}  axis = { products.slice(0, 8).map( lista => lista.main.temp - 273.15)}/>}               
+        </div>
     );
   }
 }
